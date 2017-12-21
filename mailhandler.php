2e8bcebf02ac79b1b8ddf022d4f5f62b2da6c4c8
badrinath04 <badrinathbonthu04@gmail.com>
2017-12-21 16:27:37 +0530

    Update mailhandler.php

--- mailhandler.php
+++ mailhandler.php
@@ -3,11 +3,11 @@
 		$hall=$_POST['hall'];
 		$block=$_POST['block'];
 		$name=$_POST['name'];
-		$email='badrinathbonthu05@gmail.com';
+		$email='useremail@gmail.com';
 		$phone=$_POST['phone'];
 		$msg=$_POST['msg'];
 
-		$to='badrinathbonthu04@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$to='destination@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='New Complaint';
 		$message="Hall No :".$hall."\n"."Block No :".$block."\n"."Room No :".$name."\n"."Phone :".$phone."\n"."Complaint :"."\n\n".$msg;
 		$headers="From: ".$email;
