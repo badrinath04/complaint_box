f2cd93232a691b4f4c0baf4930becc24528a0404
badrinath04 <badrinathbonthu04@gmail.com>
2017-12-21 16:30:03 +0530

    Update server.php

--- server.php
+++ server.php
@@ -8,7 +8,7 @@ $errors = array();
 $_SESSION['success'] = "";
 
 // connect to database
-$db = mysqli_connect('localhost', 'id3916344_20150649', 'asdfghjkl', 'id3916344_contactform');
+$db = mysqli_connect('localhost', 'database_user_id', '', 'database_name');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -30,7 +30,7 @@ if (isset($_POST['reg_user'])) {
 	// register user if there are no errors in the form
 	if (count($errors) == 0) {
 		$password = md5($password_1);//encrypt the password before saving in the database
-		$query = "INSERT INTO id3916344_contactform (username, email, password) 
+		$query = "INSERT INTO users (username, email, password) 
 				  VALUES('$username', '$email', '$password')";
 		mysqli_query($db, $query);
 
@@ -55,7 +55,7 @@ if (isset($_POST['login_user'])) {
 
 	if (count($errors) == 0) {
 		$password = md5($password);
-		$query = "SELECT * FROM id3916344_contactform WHERE username='$username' AND password='$password'";
+		$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 		$results = mysqli_query($db, $query);
 
 		if (mysqli_num_rows($results) == 1) {
